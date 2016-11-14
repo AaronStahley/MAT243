34e2b73d068d18724395c49ade1885e7e37dc478
AaronStahley <stahleyaaron@yahoo.com>
2016-11-14 12:34:08 -0700

    Created all 6 vaiables possibilities

--- Code/Gates/gates.java
+++ Code/Gates/gates.java
@@ -52,12 +52,33 @@ public class gates {
 		return output; 
 	}
 	
+	public static int XORGate(int input1, int input2){
+		
+		int output = 0;
+		
+		return output; 
+		
+	}
+	
+	public static int implies(int input1, int input2){
+		
+		int output = 0;
+		
+		return output; 
+	}
+	
+	public static int equivalance(int input1, int input2){
+		int output = 0;
+		
+		return output; 
+	}
+	
 	public static void evaluate(int num, gates i){
 		
 		if(num == 1){
 			   for (int a =0; a <= 1; a++ ) {
 		                    aa = (a==1)? 1: 0;
-		                    System.out.println(aa); 
+//		                    System.out.println(aa); 
 		            }
 		}
 		else if(num == 2){
@@ -66,12 +87,79 @@ public class gates {
 		            for (int b=0; b <= 1; b++ ){
 		                    int aa = (a==1)? 1: 0;
 		                    int bb = (b==1)? 1: 0;
-		                    System.out.println(aa + " " + bb + " | " + i ); 
+//		                    System.out.println(aa + " " + bb + " | " + i ); 
 
 		            }
 			
 		}
 		else if(num == 3){
+		
+			 for (int a =0; a <= 1; a++ ) 
+		            for (int b=0; b <= 1; b++ ){
+		            	for(int c=0; c <= 1; c++){
+		                    int aa = (a==1)? 1: 0;
+		                    int bb = (b==1)? 1: 0;
+		                    int cc = (b==1)? 1: 0; 
+//		                    System.out.println(aa + " " + bb + " | " + i ); 
+		            	}
+		            }
+			
+		}
+		else if(num == 4){
+			
+			 for (int a =0; a <= 1; a++ ) 
+		            for (int b=0; b <= 1; b++ ){
+		            	for(int c=0; c <= 1; c++){
+		            		for(int d=0; d <= 1; d++){
+		            			int aa = (a==1)? 1: 0;
+		            			int bb = (b==1)? 1: 0;
+		            			int cc = (b==1)? 1: 0; 
+		            			int dd = (b==1)? 1: 0;
+//		                    System.out.println(aa + " " + bb + " | " + i ); 
+		            		}
+		            	}
+		            }
+			
+		}
+		else if(num == 5){
+			
+			 for (int a =0; a <= 1; a++ ) 
+		            for (int b=0; b <= 1; b++ ){
+		            	for(int c=0; c <= 1; c++){
+		            		for(int d=0; d <= 1; d++){
+		            			for(int e=0; e <=1; e++){
+		            				int aa = (a==1)? 1: 0;
+		            				int bb = (b==1)? 1: 0;
+		            				int cc = (b==1)? 1: 0; 
+		            				int dd = (b==1)? 1: 0;
+		            				int ee = (e==1)? 1: 0;
+//		                    		System.out.println(aa + " " + bb + " | " + i ); 
+		            			}
+		            		}
+		            	}
+		            }
+			
+		}
+		else if(num == 6){
+			
+			 for (int a =0; a <= 1; a++ ) 
+		            for (int b=0; b <= 1; b++ ){
+		            	for(int c=0; c <= 1; c++){
+		            		for(int d=0; d <= 1; d++){
+		            			for(int e=0; e <=1; e++){
+		            				for(int f=0; f <=1; f++){
+		            				int aa = (a==1)? 1: 0;
+		            				int bb = (b==1)? 1: 0;
+		            				int cc = (b==1)? 1: 0; 
+		            				int dd = (b==1)? 1: 0;
+		            				int ee = (e==1)? 1: 0;
+		            				int ff = (f==1)? 1: 0; 
+//		                    		System.out.println(aa + " " + bb + " | " + i ); 
+		            				}
+		            			}
+		            		}
+		            	}
+		            }
 			
 		}
 		
